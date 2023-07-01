98204f59c6f60cf9e3beeb3b9d6dd2545fd6f9c3
Viktoria Rosovska <torimeister@gmail.com>
2023-07-01 16:40:42 +0300

    task-3

--- src/components/ForbesListItem/ForbesListItem.jsx
+++ src/components/ForbesListItem/ForbesListItem.jsx
@@ -1,9 +1,26 @@
+import PropTypes from 'prop-types';
 import { BiDollarCircle } from 'react-icons/bi';
 import { FcBullish, FcBearish } from 'react-icons/fc';
 import { ForbesItem, Avatar, Name, Capital } from './ForbesListItem.styled';
 
 import { theme } from 'styles/theme';
 
-export const ForbesListItem = () => {
-  return <div>ForbesItem</div>;
+export const ForbesListItem = ({ name, capital, avatar, isIncrease }) => {
+  return (
+    <ForbesItem>
+      <Avatar src={avatar} alt={name} />
+      <Name>{name}</Name>
+      <Capital>
+        {capital} <BiDollarCircle color={theme.colors.accent} size={22} />
+        {isIncrease ? <FcBullish /> : <FcBearish />}
+      </Capital>
+    </ForbesItem>
+  );
+};
+
+ForbesListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  capital: PropTypes.number.isRequired,
+  avatar: PropTypes.string.isRequired,
+  isIncrease: PropTypes.bool.isRequired,
 };
