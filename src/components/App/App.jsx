d08ff6a6dd0bc065739a7ed701c68bce3a3a44f7
immortalua96 <romanshvagla@icloud.com>
2023-07-01 13:59:47 +0200

    finish task-1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ import {
   ForbesList,
   CryptoHistory,
 } from 'components';
-
+import article from 'data/article.json'
 export const App = () => {
   return (
     <Section>
@@ -15,7 +15,15 @@ export const App = () => {
         <Heading marginBottom="50px" textAlign="center">
           Task 1
         </Heading>
-        <BlogCard />
+        <BlogCard
+  poster={article.poster}
+  tag={article.tag}
+  title={article.title}
+  description={article.description}
+  userName={article.name}
+  avatar={article.avatar}
+  postedAt={article.postedAt}
+/>
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 2
--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,43 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import PropTypes from 'prop-types'
+
+
+import { Avatar, Card, CardBody, CardFooter, CardHeader, CardPoster, CardText, CardTitle, Tag, UserBox, UserInfo, UserName ,Date} from "./BlogCard.styled";
+import { formatDate } from 'utilites/FormatDate/FormatDate';
+
+
+export const BlogCard = ({poster,tag,title,description,userName,avatar,postedAt}) => {
+  return <Card>
+  <CardHeader>
+    <CardPoster
+      src={poster}
+      alt={tag}
+    />
+  </CardHeader>
+  <CardBody>
+    <Tag>{tag}</Tag>
+    <CardTitle>{title}</CardTitle>
+    <CardText>
+      {description}
+    </CardText>
+  </CardBody>
+  <CardFooter>
+    <UserBox>
+      <Avatar src={avatar} alt={userName} />
+      <UserInfo>
+        <UserName>{userName}</UserName>
+        <Date>{formatDate(postedAt)}</Date>
+      </UserInfo>
+    </UserBox>
+  </CardFooter>
+</Card>
 };
+
+BlogCard.propTypes={
+  poster:PropTypes.string.isRequired,
+  tag:PropTypes.string.isRequired,
+  title:PropTypes.string.isRequired,
+  description:PropTypes.string.isRequired,
+  userName:PropTypes.string.isRequired,
+  avatar:PropTypes.string.isRequired,
+  postedAt:PropTypes.string.isRequired,
+}
